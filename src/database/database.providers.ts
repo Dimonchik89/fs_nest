b1565c9bbf2b5b78161f2e1373b233d6eec7b327
Dmitriy Kislitsyn <dima.music1989@gmail.com>
2025-10-22 11:16:12 +0300

    change database provider, add cred

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -3,17 +3,80 @@ import { User } from '../entities/user.entity';
 import { File } from '../entities/file.entity';
 import { Post } from '../entities/post.entity';
 
+// ----------------------------------------- DEV MODE
+// export const databaseProviders = [
+// 	{
+// 		provide: 'SEQUELIZE',
+// 		useFactory: async () => {
+// 			const sequelize = new Sequelize({
+// 				dialect: 'postgres',
+// 				host: process.env.POSTGRES_HOST,
+// 				port: Number(process.env.POSTGRES_PORT),
+// 				username: process.env.POSTGRES_USER,
+// 				password: process.env.POSTGRES_PASSWORD,
+// 				database: process.env.POSTGRES_DB,
+// 				dialectOptions: {
+// 					ssl: {
+// 						require: true,
+// 						rejectUnauthorized: false,
+// 					},
+// 				},
+// 				logging: false,
+// 			});
+// 			sequelize.addModels([User, File, Post]);
+// 			// sequelize.addModels([__dirname + '../**/*.entity{.ts,.js}']); work with typeorm
+// 			await sequelize.sync();
+// 			return sequelize;
+// 		},
+// 	},
+// ];
+
+// --------------------------------------- PROD MODE
+// export const databaseProviders = [
+// 	{
+// 		provide: 'SEQUELIZE',
+// 		useFactory: async () => {
+// 			const sequelize = new Sequelize({
+// 				dialect: 'postgres',
+// 				host: process.env.POSTGRES_HOST,
+// 				port: Number(process.env.POSTGRES_PORT),
+// 				username: process.env.POSTGRES_USER,
+// 				password: process.env.POSTGRES_PASSWORD,
+// 				database: process.env.POSTGRES_DB,
+// 				dialectOptions: {
+// 					ssl: {
+// 						require: true,
+// 						rejectUnauthorized: false,
+// 					},
+// 				},
+// 				logging: false,
+// 			});
+// 			sequelize.addModels([User, File, Post]);
+// 			// sequelize.addModels([__dirname + '../**/*.entity{.ts,.js}']); work with typeorm
+// 			await sequelize.sync();
+// 			return sequelize;
+// 		},
+// 	},
+// ];
+
 export const databaseProviders = [
 	{
 		provide: 'SEQUELIZE',
 		useFactory: async () => {
 			const sequelize = new Sequelize({
 				dialect: 'postgres',
-				host: process.env.POSTGRES_HOST,
-				port: Number(process.env.POSTGRES_PORT),
-				username: process.env.POSTGRES_USER,
-				password: process.env.POSTGRES_PASSWORD,
-				database: process.env.POSTGRES_DB,
+				host: 'ep-snowy-morning-a9stup3p-pooler.gwc.azure.neon.tech',
+				port: 5432,
+				username: 'neondb_owner',
+				password: 'npg_dqbAcw1F7zin',
+				database: 'neondb',
+				dialectOptions: {
+					ssl: {
+						require: true,
+						rejectUnauthorized: false,
+					},
+				},
+				logging: false,
 			});
 			sequelize.addModels([User, File, Post]);
 			// sequelize.addModels([__dirname + '../**/*.entity{.ts,.js}']); work with typeorm
